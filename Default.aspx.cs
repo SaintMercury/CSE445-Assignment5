271bd38cf05b6347a16562368a9ffc802caee52c
SAINTMERCURY\maxhe <maxHenzerling@gmail.com>
2017-11-16 20:34:12 -0700

    Did some stuff, yeah whatever

--- Default.aspx.cs
+++ Default.aspx.cs
@@ -27,16 +27,30 @@ public partial class _Default : System.Web.UI.Page
     protected void Button2_Click(object sender, EventArgs e)
     {
         // Login as a member
+        // Verify stuff
+
+        // Cookie storage
+        HttpCookie cookies = new HttpCookie("CSE445-Assignment5-Group16");
+        cookies["mName"] = mUsername.Text;
+        cookies.Expires = DateTime.Now.AddMonths(1);
+        Response.Cookies.Add(cookies);
     }
 
     protected void Button3_Click(object sender, EventArgs e)
     {
         // Login as an Admin
+        // Verify Stuff
+
+        // Cookie storage
+        HttpCookie cookies = new HttpCookie("CSE445-Assignment5-Group16");
+        cookies["aName"] = mUsername.Text;
+        cookies.Expires = DateTime.Now.AddMonths(1);
+        Response.Cookies.Add(cookies);
     }
 
     protected void Button4_Click(object sender, EventArgs e)
     {
         // Register Account Button
-
+        Response.Redirect("Register.aspx");
     }
 }
