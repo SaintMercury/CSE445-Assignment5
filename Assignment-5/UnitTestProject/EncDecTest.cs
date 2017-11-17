53bdb996bb8a4bcd80120dae266be84c12752bbb
Josh Mulla <josh.mulla1@gmail.com>
2017-11-17 12:31:29 -0700

    Solution with enc/dec, xml ops, and basic credentials manager

---
+++ Assignment-5/UnitTestProject/EncDecTest.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using EncryptionLibrary;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTestProject
+{
+    /// <summary>
+    /// Summary description for EncDecTest
+    /// </summary>
+    [TestClass]
+    public class EncDecTest
+    {
+        public EncDecTest()
+        {
+            //
+            // TODO: Add constructor logic here
+            //
+        }
+
+
+        #region Additional test attributes
+        //
+        // You can use the following additional attributes as you write your tests:
+        //
+        // Use ClassInitialize to run code before running the first test in the class
+        // [ClassInitialize()]
+        // public static void MyClassInitialize(TestContext testContext) { }
+        //
+        // Use ClassCleanup to run code after all tests in a class have run
+        // [ClassCleanup()]
+        // public static void MyClassCleanup() { }
+        //
+        // Use TestInitialize to run code before running each test 
+        // [TestInitialize()]
+        // public void MyTestInitialize() { }
+        //
+        // Use TestCleanup to run code after each test has run
+        // [TestCleanup()]
+        // public void MyTestCleanup() { }
+        //
+        #endregion
+
+        [TestMethod]
+        public void EncrpytionGeneratesExpectedValue()
+        {
+            const string expectedValue = "u6G0Wwyk2G/2TRTKSpR+9w==";
+            string plainText = "testUserName";
+            string encrypted = EncDec.Encrypt(plainText);
+            Assert.AreEqual(encrypted, expectedValue);
+        }
+
+        [TestMethod]
+        public void DecryptionGeneratesExpectedValue()
+        {
+            const string expectedValue = "testUserName";
+            string encryptedText = "u6G0Wwyk2G/2TRTKSpR+9w==";
+            string decryptedTxt = EncDec.Decrypt(encryptedText);
+            Assert.AreEqual(decryptedTxt, expectedValue);
+        }
+    }
+}
