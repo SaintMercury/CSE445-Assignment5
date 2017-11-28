a1c2eb7bc16cdaaeec752a20de0579a8a2c3bf50
SAINTMERCURY\maxhe <maxHenzerling@gmail.com>
2017-11-27 20:16:59 -0700

    Added session usage to char maker page

--- Assignment-5/Assignment-5/ProtectedMember/CharacterMaker.aspx.cs
+++ Assignment-5/Assignment-5/ProtectedMember/CharacterMaker.aspx.cs
@@ -10,9 +10,19 @@ namespace Assignment_5.ProtectedMember
 {
     public partial class CharacterMaker : System.Web.UI.Page
     {
+        List<string> characters;
+
         protected void Page_Load(object sender, EventArgs e)
         {
+            if(Session["Characters"] == null)
+            {
+                Session["Characters"] = new List<string>();
+            }
 
+            characters = (List<string>)Session["Characters"];
+            this.outputBox.Text = "";
+            foreach (var chr in characters)
+                this.outputBox.Text += chr;
         }
 
         protected void makeCharacter(object sender, EventArgs e)
@@ -21,7 +31,10 @@ namespace Assignment_5.ProtectedMember
             string str = service.getChar(this.inputName.Text);
             str = str.Replace("\\n", "\n");
             str = str.Replace("\\t", "\t");
-            this.outputBox.Text = str;
+            characters.Add(str);
+            this.outputBox.Text = "";
+            foreach (var chr in characters)
+                this.outputBox.Text += chr;
         }
     }
 }
